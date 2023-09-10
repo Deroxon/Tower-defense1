2e52666fd92d8531726031ad9d11c7727f3dd13a
Deroxon <55814631+Deroxon@users.noreply.github.com>
2023-09-10 11:42:00 +0200

    waypoints
    added waypoints

--- Tower-defense1/Assets/Scripts/enemysMove/zombieMove.cs
+++ Tower-defense1/Assets/Scripts/enemysMove/zombieMove.cs
@@ -4,19 +4,55 @@ using UnityEngine;
 
 public class zombieMove : MonoBehaviour
 {
-    public float moveSpeed = 15;
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
 
-    
-    void Start()
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+    // Use this for initialization
+    private void Start()
     {
-        
+
+        // Set position of Enemy as position of the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.position = transform.position + (Vector3.down * moveSpeed) * Time.deltaTime;
 
-      
+        // Move Enemy
+        Move();
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector3.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
     }
 }

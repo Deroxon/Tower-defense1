e9465f8b8761dd20a1b62183d7a11ba589389389
Deroxon <55814631+Deroxon@users.noreply.github.com>
2023-09-12 16:11:39 +0200

    added buyMechanic

---
+++ Tower-defense1/Assets/Scripts/towers/buyTower.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buyTower : MonoBehaviour
+{
+
+    public GameObject buildPlace;
+    public GameObject optionBuyTree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnMouseDown()
+    {
+        buildPlace.GetComponent<buildTower>().createTower("treeTower");
+        //Debug.Log("kup drewno");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

4b70f1eea87326631ac628cdca4d81b7874624bd
Deroxon <55814631+Deroxon@users.noreply.github.com>
2023-09-13 15:10:20 +0200

    added animation for build field

--- Tower-defense1/Assets/Scripts/towers/ChangeBuildingArea.cs
+++ Tower-defense1/Assets/Scripts/towers/ChangeBuildingArea.cs
@@ -8,21 +8,23 @@ public class ChangeBuildingArea : MonoBehaviour
 
     public Sprite newSprite;
     public Sprite originalSprite;
+    public SpriteRenderer spriteRenderer;
 
     private void Start()
     {
-        originalSprite = GetComponent<Image>().sprite;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
     private void OnMouseEnter() 
     {
-        GetComponent<Image> ().sprite = newSprite;
+        spriteRenderer.sprite = newSprite;
+        
     }
 
     // Update is called once per frame
     private void OnMouseExit() 
     {
-        GetComponent<Image>().sprite = originalSprite;
+        spriteRenderer.sprite = originalSprite;
     }
 }
